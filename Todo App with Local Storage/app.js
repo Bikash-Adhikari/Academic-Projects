dd05817dc9f32f97377bd5a5b38c30eb67b1df52
Bikash-Adhikari <virtual.bikash@gmail.com>
2025-03-23 12:34:38 +0545

    Handling local storage and DOM event: completed

--- Todo App with Local Storage/app.js
+++ Todo App with Local Storage/app.js
@@ -1,23 +1,68 @@
-const todoInput = document.getElementById('todo-input');
-const addTaskButton = document.getElementById('add-task-btn');
-const todoList = document.getElementById('todo-list');
+document.addEventListener('DOMContentLoaded', () => {
 
 
-let tasks = [];
+
+    const todoInput = document.getElementById('todo-input');
+    const addTaskButton = document.getElementById('add-task-btn');
+    const todoList = document.getElementById('todo-list');
 
 
-addTaskButton.addEventListener('click', () => {
-    const taskText = todoInput.value.trim();
-    if (taskText === '') return;
 
-    const newTask = {
-        id: Date.now(),
-        text: taskText,
-        completed: false
+    let strObj = localStorage.getItem('tasks');
+    let tasks = JSON.parse(strObj) || [];
+
+
+
+
+    tasks.forEach(task => renderTask(task));
+
+
+
+
+    addTaskButton.addEventListener('click', () => {
+        const taskText = todoInput.value.trim();
+        if (taskText === '') return;
+
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(newTask);
+        saveTasks();
+        todoInput.value = "";
+    })
+
+
+
+
+    function renderTask(task) {
+        console.log(task);
     }
 
 
-    tasks.push(newTask);
-    todoInput.value = "";
-    console.log(tasks);
+
+
+
+
+    // Save the Tasks to local storage
+
+    function saveTasks() {
+
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
